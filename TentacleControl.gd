eb7ef2655eb80c1b93559c2ad4c858589ea62272
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2017-02-11 15:54:20 -0800

    seperate scene quadupus

--- TentacleControl.gd
+++ TentacleControl.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 export var rotationLimit = 1.5
 export var rotationSpeed = 1
+export var moveSpeed = Vector2(1,0);
+
 func _ready():
 	self.set_process(true)
 	self.set_process_input(true)
@@ -10,6 +12,9 @@ func _ready():
 	pass
 
 func _process(delta):
+	if(Input.is_action_pressed("ui_right")):
+		set_pos(get_pos()+moveSpeed)
+		Camera2D.set_pos(get_pos()+moveSpeed)
 	if(Input.is_action_pressed("p1_radial_movement_a")):
 		if(Input.get_joy_axis(0,0) < 0 &&  get_node("BodySprite/TentacleAJoint").get_rot() > -rotationLimit):
 			get_node("BodySprite/TentacleAJoint").rotate(Input.get_joy_axis(0,0)*delta*rotationSpeed)
