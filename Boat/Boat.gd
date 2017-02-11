ad0d34b82784ce48c63bea0db7f1548930d9ec37
Dan <danr94@gmail.com>
2017-02-11 15:56:29 -0800

    Boat script that sets up the shader.

---
+++ Boat/Boat.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+export var mask_viewport_path = NodePath()
+export var sprite_path = NodePath()
+
+func _ready():
+	var mask_viewport = get_node(mask_viewport_path)
+	var sprite = get_node(sprite_path)
+	var mask_texture = mask_viewport.get_render_target_texture()
+	get_material().set_shader_param("Mask", mask_texture)
+
