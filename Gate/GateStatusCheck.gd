d96ba5d929e7ca070ec0426789278d1500adcacb
Dan <danr94@gmail.com>
2017-02-12 12:48:03 -0800

    Update the gate collision so success and failure are detected.

---
+++ Gate/GateStatusCheck.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var checked = false
+var start_x
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if !checked:
+		var player = get_node("/root/Game/Quadupus")
+		
+		var gate_x = get_global_pos().x
+		var player_x = player.get_global_pos().x
+		
+		var gate_pieces = get_tree().get_nodes_in_group("gate_piece")
+		
+		var touch_count = 0
+		
+		for piece in gate_pieces:
+			if piece.is_touching():
+				touch_count += 1
+			
+		if touch_count >= 5:
+			print("Everything is touching")
+			set_z(10) #Set the z to a high number so that the player appears to go behind it
+			checked = true
+		
+		if player_x >= gate_x:
+			print("Passed")
+			checked = true
