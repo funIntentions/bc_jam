c228b189b8178ada3ab6b764c5aac2e8609b312a
Dan <danr94@gmail.com>
2017-02-12 12:02:12 -0800

    Updated gate collision checks.

--- Gate/GateKeeper.gd
+++ Gate/GateKeeper.gd
@@ -1,17 +1,27 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var checked = false
+var start_x
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	var gate_pieces = get_tree().get_nodes_in_group("gate_piece")
-	
-	for piece in gate_pieces:
-		if !piece.is_touching():
-			return;
-	
-	print("Everything is touching")
+	if !checked:
+		var player = get_node("/root/Game/Quadupus")
+		
+		var gate_x = get_pos().x
+		var player_x = player.get_pos().x
+		
+		if player_x >= gate_x:
+			print("passed")
+			var gate_pieces = get_tree().get_nodes_in_group("gate_piece")
+			
+			var touch_count = 0
+			
+			for piece in gate_pieces:
+				if piece.is_touching():
+					touch_count += 1
+			
+			if touch_count >= 5:
+				print("Everything is touching")
