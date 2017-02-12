3610ccda3118b158359e1427e9c2f31c29bb33f4
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2017-02-12 11:37:33 -0800

    Changed quadupus z to 0 and made seaweed spawn wait random time between 6 and 10 seconds

--- SeaweedSpawner.gd
+++ SeaweedSpawner.gd
@@ -2,14 +2,16 @@ extends Node2D
 
 export (PackedScene) var seaweed
 var timer
+var randomTime = 6
 
 func _ready():
 	timer = get_node("Timer")
 	timer.connect("timeout", self, "spawnSeaweed")
-	timer.set_wait_time(6)
+	timer.set_wait_time(randomTime)
 	timer.start()
 
 func spawnSeaweed():
 	add_child(seaweed.instance())
 	var camPosition = (get_parent().get_child(0).get_pos())
 	set_pos(Vector2(camPosition.x + 600, camPosition.y + (rand_range(-500,100))))
+	timer.set_wait_time(rand_range(6,10))
